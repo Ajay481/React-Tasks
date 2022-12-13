f3adf6f2f0f7fbe1d3557c780212b4f39aeae4ec
Ajay481 <iamajayyadav481@gmail.com>
2022-12-13 12:34:44 +0530

    Expense Items

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/ExpenseItem'
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <ExpenseItem></ExpenseItem>
         <a
           className="App-link"
           href="https://reactjs.org"
