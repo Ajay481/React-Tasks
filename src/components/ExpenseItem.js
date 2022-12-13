b45afdca004ccbcd917cf5ad76466fd96d15b5d1
Ajay481 <iamajayyadav481@gmail.com>
2022-12-13 19:51:54 +0530

    Split components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,14 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
-        <div className="expense-item h2">{props.LocationofExpenditure}</div>
-      </div>
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails title={props.title} />
+      <ExpenseDetails amount={props.amount} />
+      <ExpenseDetails title={props.LocationofExpenditure} />   
     </div>
   );
 }
