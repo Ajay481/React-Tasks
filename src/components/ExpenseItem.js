cf8c25c31e0277b70b8b3093a277b1b47de48900
Ajay481 <iamajayyadav481@gmail.com>
2022-12-13 17:56:49 +0530

    Loop added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,13 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = "Rs 4600";
-  const LocationofExpenditure = "Location of Expenditure";
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
-        <div className="expense-item h2">{LocationofExpenditure}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+        <div className="expense-item h2">{props.LocationofExpenditure}</div>
       </div>
     </div>
   );
