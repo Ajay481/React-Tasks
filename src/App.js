a21589188a1dbd0686db1735d866b84151724531
Ajay481 <iamajayyadav481@gmail.com>
2022-12-18 12:33:17 +0530

    Delete Expense Added

--- src/App.js
+++ src/App.js
@@ -45,34 +45,38 @@ const App = () => {
     //     })}
     //   </div>
     // </div>
- 
-<Card className="expenses">
- <ExpenseItem
-    title={expenses[0].title}
-    amount={expenses[0].amount}
-    date={expenses[0].date}
-    LocationofExpenditure={expenses[0].LocationofExpenditure}
-  ></ExpenseItem>
-  <ExpenseItem
-    title={expenses[1].title}
-    amount={expenses[1].amount}
-    date={expenses[1].date}
-    LocationofExpenditure={expenses[1].LocationofExpenditure}
-  ></ExpenseItem>
-  <ExpenseItem
-    title={expenses[2].title}
-    amount={expenses[2].amount}
-    date={expenses[2].date}
-    LocationofExpenditure={expenses[2].LocationofExpenditure}
-  ></ExpenseItem>
-  <ExpenseItem
-    title={expenses[3].title}
-    amount={expenses[3].amount}
-    date={expenses[3].date}
-    LocationofExpenditure={expenses[3].LocationofExpenditure}
-  ></ExpenseItem> 
-  </Card>
+
+    <Card className="expenses">
+      <ExpenseItem
+        id={expenses[0].id}
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+        LocationofExpenditure={expenses[0].LocationofExpenditure}
+      ></ExpenseItem>
+      <ExpenseItem
+        id={expenses[1].id}
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+        LocationofExpenditure={expenses[1].LocationofExpenditure}
+      ></ExpenseItem>
+      <ExpenseItem
+        id={expenses[2].id}
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+        LocationofExpenditure={expenses[2].LocationofExpenditure}
+      ></ExpenseItem>
+      <ExpenseItem
+        id={expenses[3].id}
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+        LocationofExpenditure={expenses[3].LocationofExpenditure}
+      ></ExpenseItem>
+    </Card>
   );
-}
+};
 
 export default App;
--- src/Expenses/ExpenseItem.js
+++ src/Expenses/ExpenseItem.js
@@ -4,13 +4,21 @@ import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 
 const ExpenseItem = (props) => {
+  const clickHandler = (id) => {
+    console.log(id);
+    const element = document.getElementById(id);
+    element.remove();
+  };
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <ExpenseDetails title={props.title} />
-      <ExpenseDetails amount={props.amount} />
+    <Card>
+      <div className="expense-item" id={props.id}>
+        <ExpenseDate date={props.date} />
+        <ExpenseDetails title={props.title} />
+        <ExpenseDetails amount={props.amount} />
+        <button onClick={() => clickHandler(props.id)}>Delete Expense</button>
+      </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
